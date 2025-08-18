a9f811a82190b524a393a0201ba62129d5f46cd4
Zeke <ezequielgerstel777@gmail.com>
2025-08-18 13:32:25 -0400

    Note entity created and refactor

---
+++ app/src/main/java/com/example/gradeprojectdb01/data/entities/Note.kt
@@ -0,0 +1,15 @@
+package com.example.gradeprojectdb01.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    @PrimaryKey (autoGenerate = true)
+    val noteId: Int,
+    val frequency: Double,
+    val name: String,
+    val alteration: String,
+    val octave: Int,
+    val relationToReference: Double
+)
