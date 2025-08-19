3e7d436a66018821efe746273e9a25ec0a788aa1
Zeke <ezequielgerstel777@gmail.com>
2025-08-19 10:44:58 -0400

    Note dao methods declaration (non exhaustive)

--- app/src/main/java/com/example/gradeprojectdb01/data/daos/NoteDao.kt
+++ app/src/main/java/com/example/gradeprojectdb01/data/daos/NoteDao.kt
@@ -1,4 +1,18 @@
 package com.example.gradeprojectdb01.data.daos
 
+import androidx.lifecycle.LiveData
+import com.example.gradeprojectdb01.data.entities.Note
+
 interface NoteDao {
+    suspend fun addNote(note: Note)
+
+    fun readAllNotesLive(): LiveData<List<Note>>
+
+    suspend fun readAllNotes(): List<Note>
+
+    suspend fun updateNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+
+    suspend fun deleteAllNotes()
 }
