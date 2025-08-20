f9a90d811e74029b751a4d7ff8dc955b2d8857c6
Zeke <ezequielgerstel777@gmail.com>
2025-08-20 11:15:29 -0400

    Basic NoteDao operations

--- app/src/main/java/com/example/gradeprojectdb01/data/daos/NoteDao.kt
+++ app/src/main/java/com/example/gradeprojectdb01/data/daos/NoteDao.kt
@@ -1,18 +1,49 @@
 package com.example.gradeprojectdb01.data.daos
 
 import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
 import com.example.gradeprojectdb01.data.entities.Note
+import com.example.gradeprojectdb01.data.relations.NoteWithInstruments
+import com.example.gradeprojectdb01.data.relations.NoteWithTuningSystems
+import kotlinx.coroutines.flow.Flow
 
 interface NoteDao {
+    @Insert(onConflict= OnConflictStrategy.REPLACE)
     suspend fun addNote(note: Note)
 
-    fun readAllNotesLive(): LiveData<List<Note>>
+    @Query("SELECT * FROM Note ORDER BY noteId ASC")
+    fun observeAllNotes(): Flow<List<Note>>
 
-    suspend fun readAllNotes(): List<Note>
+    @Query("SELECT * FROM Note ORDER BY noteId ASC")
+    suspend fun getAllNotesOnce(): List<Note>
 
+    @Update
     suspend fun updateNote(note: Note)
 
+    @Delete
     suspend fun deleteNote(note: Note)
 
+    @Query("DELETE FROM Note")
     suspend fun deleteAllNotes()
+
+    @Transaction
+    @Query("SELECT * FROM Note WHERE noteId = :noteId")
+    suspend fun getNoteWithInstrumentsOnce(noteId:Int): List<NoteWithInstruments>
+
+    @Transaction
+    @Query("SELECT * FROM Note WHERE noteId = :noteId")
+    suspend fun getNoteWithTuningSystemsOnce(noteId:Int): List<NoteWithTuningSystems>
+
+    @Transaction
+    @Query("SELECT * FROM Note WHERE noteId = :noteId")
+    fun observeNoteWithInstruments(noteId:Int): Flow<List<NoteWithInstruments>>
+
+    @Transaction
+    @Query("SELECT * FROM Note WHERE noteId = :noteId")
+    fun observeNoteWithTuningSystems(noteId:Int): Flow<List<NoteWithTuningSystems>>
 }
