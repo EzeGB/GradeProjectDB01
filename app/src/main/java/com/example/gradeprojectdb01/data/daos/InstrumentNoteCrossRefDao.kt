ec336f6faa08ad51cbd438aeb2cf700b1bdd2f44
Zeke <ezequielgerstel777@gmail.com>
2025-08-22 13:29:31 -0400

    Cross ref Dao's implemented

--- app/src/main/java/com/example/gradeprojectdb01/data/daos/InstrumentNoteCrossRefDao.kt
+++ app/src/main/java/com/example/gradeprojectdb01/data/daos/InstrumentNoteCrossRefDao.kt
@@ -1,4 +1,38 @@
 package com.example.gradeprojectdb01.data.daos
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.gradeprojectdb01.data.entities.InstrumentNoteCrossRef
+
+@Dao
 interface InstrumentNoteCrossRefDao {
+    //CREATE BLOCK
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertInstNoteCrossRef(instNoteCrossRef: InstrumentNoteCrossRef)
+
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllInstNoteCrossRefs(instNoteCrossRef: List<InstrumentNoteCrossRef>)
+
+    //DELETE BLOCK
+    @Delete
+    suspend fun deleteInstNoteCrossRef(instNoteCrossRef: InstrumentNoteCrossRef)
+
+    @Delete
+    suspend fun deleteInstNoteCrossRefs(instNoteCrossRef: List<InstrumentNoteCrossRef>)
+
+    @Query("DELETE FROM InstrumentNoteCrossRef " +
+            "WHERE instrumentId = :instrumentId AND noteId = :noteId")
+    suspend fun deleteInstNoteCrossRefByIds(instrumentId: Long, noteId:Long)
+
+    @Query("DELETE FROM InstrumentNoteCrossRef WHERE instrumentId = :instrumentId")
+    suspend fun deleteAllInstNoteCrossRefByInstrumentId(instrumentId: Long)
+
+    @Query("DELETE FROM InstrumentNoteCrossRef WHERE noteId = :noteId")
+    suspend fun deleteAllInstNoteCrossRefByNoteId(noteId:Long)
+
+    @Query("DELETE FROM InstrumentNoteCrossRef")
+    suspend fun deleteAllInstNoteCrossRefs()
 }
